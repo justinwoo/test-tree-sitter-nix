f4037d7a84e1954928025ff9c44d3ca021a840c5
justinwoo <moomoowoo@gmail.com>
2019-06-05 01:18:12 +0300

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+const Parser = require('tree-sitter');
+const Nix = require('tree-sitter-nix');
+
+const parser = new Parser();
+parser.setLanguage(Nix);
+
+const sourceCode = 'let x = "hi"; in "yes: ${hi}"';
+const tree = parser.parse(sourceCode);
+
+console.log(tree.rootNode.toString());
