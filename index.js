b72f42427c1adaab21db2383d30dd9ee486567dd
justinwoo <moomoowoo@gmail.com>
2019-07-14 15:13:49 +0300

    oiajwefoijawef

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ const Nix = require('tree-sitter-nix');
 const parser = new Parser();
 parser.setLanguage(Nix);
 
-const sourceCode = 'let x = "hi"; in "yes: ${hi}"';
+const sourceCode = `
+let
+  x = "hi";
+  y = "no";
+  z = "wef";
+in "yes: \${hi}"
+`;
 const tree = parser.parse(sourceCode);
 
 console.log(tree.rootNode.toString());
 
-module.exports = tree
+const root = tree.rootNode;
+const let_ = root.children[0]
+
+let_.children.map((x, i) => {
+  console.log(`child ${i}`)
+  console.log(x.type)
+  console.log(x.text)
+  console.log(x.isNamed)
+  console.log()
+});
+
+module.exports = {
+  Parser, Nix, parser
+}
